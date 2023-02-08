4352e1539eba5d7b9fe159099185e2b6b0bd716b
Adeliz Keith <adelizkeith@gmail.com>
2023-02-08 17:43:22 -0500

    Make app default to fullscreen.

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
-    fullscreen: false,
+    fullscreen: true,
     show: false,
     webPreferences: {
       nodeIntegration: true,
